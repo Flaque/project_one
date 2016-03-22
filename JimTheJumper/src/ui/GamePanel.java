48358113f5fb6858b866588ba2e3e6444ed150e7
Flaque <econrad@zagmail.gonzaga.edu>
2016-03-22 14:57:12 -0700

    udpated gitignore plus some changes to window

--- JimTheJumper/src/ui/GamePanel.java
+++ JimTheJumper/src/ui/GamePanel.java
@@ -1,11 +1,7 @@
 package ui;
 
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
